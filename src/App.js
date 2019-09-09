b5aec4ab3ed46757de99f73b3156790643b50660
SFONNE <apollohattsune@gmail.com>
2019-09-09 08:15:15 +0700

    Last commi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-const word= "Hello";
-class App extends Component{
-  render() 
-  {
-    return(
-    <div>
-      {
-         Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-      }
-    </div>
+import React, { Component } from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard.js';
+import WordCard from './WordCard.js';
+
+const word = "computerengineering";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WordCard value="computerengineering" />
+      </div>
     );
   }
 }
+export default App;
--- src/CharacterCard.jsx
+++
@@ -1,7 +0,0 @@
-export default class CharacterCard extends Component{
-    render(){
-        return(
-        <div>{this.props.value}</div>
-        )
-    }
-}
