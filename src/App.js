b04b4f8d83c3d83ddd9a503a7c46becd5a243818
SFONNE <apollohattsune@gmail.com>
2019-09-09 08:59:45 +0700

    Check4 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component} from'react';
-import'./App.css';
-class App extends Component
-{render() 
-  {return(
-    <div className="App">Hello World</div>
-    )
-    ;
+import React, { Component } from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard.js';
+import WordCard from './WordCard.js';
+
+const word = "Hello";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+        Array.from(word).map((c, i) =><CharacterCard value={c}key={i}/>)
+        }
+        </div>
+    );
   }
-}export default App;
+}
+export default App;
