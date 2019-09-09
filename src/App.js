0975875c0f59e402ca348a3cc33cf8409e294e29
SFONNE <apollohattsune@gmail.com>
2019-09-09 07:47:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const word= "Hello";
+class App extends Component{
+  render() 
+  {
+    return(
+    <div>
+      {
+         Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
+    </div>
+    );
+  }
+}
